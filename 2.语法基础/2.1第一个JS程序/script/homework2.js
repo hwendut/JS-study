3ed57be9bbb1fd604987f00444d81325391f377a
hwen_Laptop <wenhaodut@163.com>
2024-03-19 10:41:50 +0800

    first commit

---
+++ 2.语法基础/2.1第一个JS程序/script/homework2.js
@@ -0,0 +1,5 @@
+document.write("hello, world");
+document.write(1);
+alert(1);
+alert("hello");
+console.log("hello");
---
+++ 2.语法基础/2.2/script/homework1.js
@@ -0,0 +1,11 @@
+console.log({
+    name: "文浩",
+    age: 25,
+    gender: true,
+    height: 171.5,
+    school: {
+        address: "No.2 Linggong Road",
+        name: "DUT"
+    },
+    girlFriend: null
+})
